52e8a3741ab6c6df9737f0181a8b357f04350cfa
Ali Erinc Ayaz <45971992+erincayaz@users.noreply.github.com>
2024-06-23 20:39:39 +0300

    Comments added for problem 1

--- problem1.py
+++ problem1.py
@@ -1,3 +1,11 @@
+# Formula used:
+# ---------------------------------------
+# Total number of natural numbers between (a, b): (b - a) / d + 1
+# d is the difference between two consecutive numbers
+# ---------------------------------------
+# Sum of all numbers between (a, b): ((a + b) * n) / 2
+# n is number of natural numbers between (a, b)
+
 sum = ((3 + 999) * ((999 - 3) / 3 + 1) + (5 + 995) * ((995 - 5) / 5 + 1) - (15 + 990) * ((990 - 15) / 15 + 1)) / 2
 
 print(int(sum))
